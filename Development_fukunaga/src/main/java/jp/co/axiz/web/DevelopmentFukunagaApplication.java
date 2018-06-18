0bd918fe351d496a1795174f04ad8417c93a8889
axizNfukunaga <18y_fukunaga@axiz.co.jp>
2018-06-18 16:08:13 +0900

    0618:16:07

---
+++ Development_fukunaga/src/main/java/jp/co/axiz/web/DevelopmentFukunagaApplication.java
@@ -0,0 +1,12 @@
+package jp.co.axiz.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentFukunagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentFukunagaApplication.class, args);
+	}
+}
